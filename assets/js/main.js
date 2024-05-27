6c3ce51d7fa8f4f450b447d5a8e580113da59b69
Manvith B Y <66794160+manvith1604@users.noreply.github.com>
2024-05-27 13:26:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@
       }
     }
   }
-
+  new cursoreffects.trailingCursor({particles: 15, rate: 0.8, baseImageSrc: "data:image/png;base64,iVB..."});
   /**
    * Easy on scroll event listener 
    */
